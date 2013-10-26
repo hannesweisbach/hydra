ea23133ddfbdf2d631cead82e798e09f25b2b005
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2013-10-26 19:48:26 +0200

    Make RDMAServer compilable

--- RDMAServer.cpp
+++ RDMAServer.cpp
@@ -1,9 +1,11 @@
 #include <netdb.h>  // getaddrinfo
 #include <assert.h>
-#include <exception>
+#include <stdexcept>
 #include <iostream>
+#include <cstring>
+#include <rdma/rdma_cma.h>
 
-#include "rdma_server.hpp"
+#include "RDMAServer.hpp"
 
 #if 0
 class RDMAConnection {
@@ -29,20 +31,19 @@ class RDMAConnection {
 };
 #endif
 
-class RDMAException : public runtime_error {
+class RDMAException : public std::runtime_error {
   public:
-    explicit RDMAException(const char * what, const char * err) : runtime_error(std::string(what) + std::string(" failed: ") + std::string(err) {}
+    explicit RDMAException(const char * what, const char * err) : runtime_error(std::string(what) + std::string(" failed: ") + std::string(err)) {}
     explicit RDMAException(const std::string& what) : runtime_error(what) {}
 };
 
 RDMAServer::RDMAServer() :
   ec(nullptr),
-  id(nullptr),
-  ev(nullptr) {}
+  id(nullptr) {}
 
-RDMAServer::RDMAServer(const uint16_t port) : RDMAServer(std::string(), port) {}
+RDMAServer::RDMAServer(const std::string& port) : RDMAServer(std::string(), port) {}
 
-RDMAServer::RDMAServer(const std::string& host, const uint16_t port) : RDMAServer() {
+RDMAServer::RDMAServer(const std::string& host, const std::string& port) : RDMAServer() {
   if((ec = rdma_create_event_channel()) == nullptr)
     throw new RDMAException("rdma_create_event_channel", strerror(errno));
   
@@ -53,15 +54,15 @@ RDMAServer::RDMAServer(const std::string& host, const uint16_t port) : RDMAServe
   const char * hostname = (host.empty()) ? nullptr : host.c_str();
 
   struct addrinfo *ais;
-  atruct addrinfo hints = {};
-  hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
+  struct addrinfo hints = {};
+  hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG | AI_NUMERICSERV;
 
-  int err = getaddrinfo(hostname, port, &hints, &ais);
+  int err = getaddrinfo(hostname, port.c_str(), &hints, &ais);
   if(err)
     throw new RDMAException("getaddrinfo", gai_strerror(err));
 
   for(struct addrinfo * ai = ais; ai != nullptr; ai = ai->ai_next) {
-    if(rdma_bind_addr(id, ai->addr))
+    if(rdma_bind_addr(id, ai->ai_addr))
       throw new RDMAException("rdma_bind_addr", strerror(errno));
   }
 
@@ -69,9 +70,6 @@ RDMAServer::RDMAServer(const std::string& host, const uint16_t port) : RDMAServe
 
   if(rdma_listen(id, 10))
     throw new RDMAException("rdma_listen", strerror(errno));
-
-  uint16_t port_assigned = ntohs(rdma_get_src_port(id));
-  assert(port_assigned == port); //TODO: throw?
 
   //TODO: logging facility
   std::cout << "Started server on port " << port;
--- RDMAServer.hpp
+++ RDMAServer.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
+#include <string>
+
 class RDMAServer {
   struct rdma_event_channel * ec = nullptr;
   struct rdma_cm_id         * id = nullptr;
-  struct rdma_cm_event      * ev = nullptr;
   
   public:
     RDMAServer();
-    RDMAServer(const uint16_t port);
-    RDMAServer(const std::string& host, const uint16_t port);
+    RDMAServer(const std::string& port);
+    RDMAServer(const std::string& host, const std::string& port);
     ~RDMAServer();
 };
 
