499d189fab7bc9170648c17f846477426bfa0e63
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-03-26 09:47:55 +0100

    Add explicit casts to silence warning

--- HashTable/Hash.hpp
+++ HashTable/Hash.hpp
@@ -20,13 +20,15 @@ class Hash {
 public:
   Hash(int power)
       : wM(std::numeric_limits<T>::digits - power),
-        a((std::mt19937_64(
-               std::chrono::system_clock::now().time_since_epoch().count())() *
-           2) -
-          1),
-        b(std::mt19937_64(
-              std::chrono::system_clock::now().time_since_epoch().count())() &
-          ((1 << wM) - 1)) {
+        a(static_cast<T>((std::mt19937_64(std::chrono::system_clock::now()
+                                              .time_since_epoch()
+                                              .count())() *
+                          2) -
+                         1)),
+        b(static_cast<T>(
+            std::mt19937_64(
+                std::chrono::system_clock::now().time_since_epoch().count())() &
+            ((1 << wM) - 1))) {
     assert(std::numeric_limits<T>::digits > power);
   }
   T hash(const T x) { return (T)(a * x + b) >> wM; }
