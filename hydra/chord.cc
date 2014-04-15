7ada951f61bb01a44eabd33c5d69be058338036e
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-04-15 18:32:19 +0200

    Clean up

--- hydra/chord.cc
+++ hydra/chord.cc
@@ -1,20 +1,13 @@
 #include "hydra/chord.h"
-#if 0
-#include "hydra/passive.h"
-#else
 #include "hydra/types.h"
 #include "hydra/messages.h"
 #include "rdma/RDMAClientSocket.h"
-#endif
 
 namespace hydra {
 namespace chord {
 static routing_table find_table(routing_table table, const keyspace_t &id) {
   while (!id.in(table.self().node.id + 1, table.successor().node.id)) {
     auto re = table.preceding_node(id).node;
-#if 0
-    hydra::passive node(re.ip, re.port);
-#else
     /* Ok. this appears also in client.cc. Wrap. */
     RDMAClientSocket socket(re.ip, re.port);
     socket.connect();
@@ -32,8 +25,6 @@ static routing_table find_table(routing_table table, const keyspace_t &id) {
         info.second.first->routing_table.rkey);
     table_.first.get();
     table = table_.second.first->get();
-#endif
-    //table = node.table();
   }
   return table;
 }
