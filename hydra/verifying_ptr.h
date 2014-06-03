5cb50c60b9b71e9f7adf83dc5c24b92b30a3bbae
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-06-03 15:17:41 +0200

    Fixup verifying_ptr
    add default ctor/make all ctor args non-default
    add operator bool
    complete reset

--- hydra/verifying_ptr.h
+++ hydra/verifying_ptr.h
@@ -10,15 +10,18 @@ template <typename T> struct verifying_ptr {
   size_t size;
   uint64_t crc;
 
-  verifying_ptr(const T *p = nullptr, size_t s = 0) noexcept
+  verifying_ptr(const T *p, size_t s) noexcept
       : ptr(reinterpret_cast<uint64_t>(p)),
         size(s),
         crc(::hydra::hash64(p, s)) {}
+  verifying_ptr() noexcept : verifying_ptr(nullptr, 0) {}
   const T *get() const { return reinterpret_cast<T *>(ptr); }
   bool is_empty() const { return reinterpret_cast<T *>(ptr) == nullptr; }
+  operator bool() const { return reinterpret_cast<T *>(ptr) != nullptr; }
   void reset() {
     ptr = reinterpret_cast<uint64_t>(nullptr);
     size = 0;
+    crc = ::hydra::hash64(nullptr, 0);
   }
   //  void update() { crc = hash64(reinterpret_cast<void*>(ptr), size); }
 };
