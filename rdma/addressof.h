b1c105a9e271de7cf81e45aeccfb726c0df1ecc1
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-03 15:00:57 +0200

    Add address_of/size_of functions
    They are used to get the address and size of objects intended to be
    registered for RDMA. Users can overload/specialize for their own types.
    An overload for std::vector already exists.

---
+++ rdma/addressof.h
@@ -0,0 +1,38 @@
+#pragma once
+
+namespace hydra {
+namespace rdma {
+
+namespace _ {
+template <typename T> const void *address_of(const T &o, std::true_type) {
+  return o;
+}
+template <typename T> const void *address_of(const T &o, std::false_type) {
+  return &o;
+}
+
+template <typename T> size_t size_of(const T &, std::true_type) {
+  return sizeof(std::remove_pointer<T>::type);
+}
+template <typename T> size_t size_of(const T &, std::false_type) {
+  return sizeof(T);
+}
+}
+
+template <typename T> const void *address_of(const T &o) {
+  return _::address_of(o, std::is_pointer<T>());
+}
+
+template <typename T> const void *address_of(const std::vector<T> &o) {
+  return o.data();
+}
+
+template <typename T> size_t size_of(const T &o) {
+  return _::size_of<T>(o, std::is_pointer<T>());
+}
+
+template <typename T> size_t size_of(const std::vector<T> &o) {
+  return o.size() * sizeof(T);
+}
+}
+}
