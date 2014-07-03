28be60738f78cfd18c6fefb5d183f7e409216ebd
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-03 18:33:44 +0200

    Migrate put_loop to capnp

--- benchmarks/dht/put_loop.cc
+++ benchmarks/dht/put_loop.cc
@@ -3,19 +3,32 @@
 #include <atomic>
 #include <thread>
 
+#include <capnp/message.h>
+#include <capnp/serialize.h>
+#include "dht.capnp.h"
+
 #include "hydra/client.h"
 #include "hydra/hash.h"
 
 #include "util/Logger.h"
 
-#include "google/profiler.h"
+namespace hydra {
+namespace rdma {
+template <> const void *address_of(const kj::Array<capnp::word> &o) {
+  return o.begin();
+}
+
+template <> size_t size_of(const kj::Array<capnp::word> &o) {
+  return o.size() * sizeof(capnp::word);
+}
+}
+}
 
 struct data {
   rdma_ptr<unsigned char> key;
   rdma_ptr<unsigned char> value;
   size_t key_length;
   size_t value_length;
-  std::pair<std::future<qp_t>, rdma_ptr<put_response> > result;
   data(rdma_ptr<unsigned char> key, rdma_ptr<unsigned char> value,
        const size_t key_length, const size_t value_length)
       : key(std::move(key)), value(std::move(value)), key_length(key_length),
@@ -23,18 +36,38 @@ struct data {
 };
 
 static void send(RDMAClientSocket &socket, data &request) {
-  put_request r = { { request.key.first.get(), request.key_length,
-                      request.key.second->rkey },
-                    { request.value.first.get(), request.value_length,
-                      request.value.second->rkey } };
+  ::capnp::MallocMessageBuilder message;
+  hydra::protocol::DHTRequest::Builder msg =
+      message.initRoot<hydra::protocol::DHTRequest>();
 
-  request.result = socket.recv_async<put_response>();
-  socket.sendImmediate(r);
+  auto put = msg.initPut();
+  auto remote = put.initRemote();
+  auto key = remote.initKey();
+  key.setAddr(reinterpret_cast<uint64_t>(request.key.first.get()));
+  key.setSize(request.key_length);
+  key.setRkey(request.key.second->rkey);
+  auto value = remote.initValue();
+  value.setAddr(reinterpret_cast<uint64_t>(request.value.first.get()));
+  value.setSize(request.value_length);
+  value.setRkey(request.value.second->rkey);
+
+  auto result = socket.recv_async<kj::FixedArray<capnp::word, 9> >();
+
+  kj::Array<capnp::word> serialized = messageToFlatArray(message);
+  socket.sendImmediate(serialized);
+  result.first.get();
+
+  auto reply = capnp::FlatArrayMessageReader(*result.second.first);
+  auto reader = reply.getRoot<hydra::protocol::DHTResponse>();
+
+  assert(reader.getAck().getSuccess());
 }
 
-static void load_keys(RDMAClientSocket &socket, const size_t max_keys,
-                      const size_t value_length, std::atomic_bool &run,
-                      std::atomic<uint64_t> &cnt) {
+static void load_keys(const std::string &host, const std::string &port,
+                      const size_t max_keys, const size_t value_length,
+                      std::atomic_bool &run, std::atomic<uint64_t> &cnt) {
+  RDMAClientSocket socket(host, port);
+  socket.connect();
   std::mt19937_64 generator;
   std::uniform_int_distribution<unsigned char> distribution(' ', '~');
 
@@ -59,45 +92,59 @@ static void load_keys(RDMAClientSocket &socket, const size_t max_keys,
     }
   }
 
-  for (auto &&request : requests) {
-    send(socket, request);
-    cnt++;
-  }
-
-  for (size_t i = 0; run.load(); i++) {
-    if (i >= requests.size())
-      i = 0;
-    auto &&request = requests[i];
-    request.result.first.get();
-    assert(request.result.second.first->value());
-    send(socket, request);
-    cnt++;
+  for (;;) {
+    for (auto &&request : requests) {
+      send(socket, request);
+      cnt++;
+      if (!run)
+        return;
+    }
   }
 }
 
-int main() {
+int main(int argc, char const *const argv[]) {
+  const size_t value_length = (argc < 2) ? 64 : atoi(argv[1]);
+  const size_t max_keys = 255;
+  const size_t min_threads = 1;
+  const size_t max_threads = 20;
+  const auto measurement_time = std::chrono::seconds(2);
+
+  static_assert(min_threads <= max_threads,
+                "The minimum number of threads must "
+                "be smaller than the maximum number");
+
   std::atomic<uint64_t> cnt(0);
-  const size_t max_keys = 512;
-  const auto measurement_time = std::chrono::seconds(20);
 
-  RDMAClientSocket socket("10.1", "8042");
-  socket.connect();
+  for (const auto &value_size : { value_length /*64, 128, 256, 1024, 4096*/ }) {
+    for (size_t cur_threads = min_threads; cur_threads <= max_threads;
+         cur_threads++) {
+      std::atomic_bool run(true);
 
-  //  ProfilerStart("./load.prof");
-  // load_keys(client, max_keys, 64);
-  for (const auto &value_size : { 64, 128, 256, 1024, 4096 }) {
-    std::atomic_bool run(true);
-    hydra::async([&]() {
-      std::this_thread::sleep_for(measurement_time);
-      run = false;
-    });
-    log_info() << "Running with a value_size of " << value_size;
-    load_keys(socket, max_keys, value_size, run, cnt);
-    const uint64_t seconds = std::chrono::duration_cast<std::chrono::seconds>(
-        measurement_time).count();
-    log_info() << "kOps/s: " << cnt.fetch_and(0) / seconds / 1000;
+      hydra::async([&]() {
+        std::this_thread::sleep_for(measurement_time);
+        run = false;
+      });
+
+      std::cout << "Running with a value_size of " << value_size << " and "
+                << cur_threads << " threads ... ";
+      std::cout.flush();
+
+      std::vector<std::thread> threads;
+      threads.reserve(cur_threads);
+
+      std::generate_n(std::back_inserter(threads), cur_threads, [&]() {
+        return std::thread(load_keys, "10.1", "8042", max_keys, value_length,
+                           std::ref(run), std::ref(cnt));
+      });
+
+      for (auto &&thread : threads) {
+        thread.join();
+      }
+
+      const uint64_t seconds = std::chrono::duration_cast<std::chrono::seconds>(
+          measurement_time).count();
+      std::cout << "kOps/s: " << cnt.fetch_and(0) / seconds / 1000 << std::endl;
+    }
   }
-
-  ProfilerStop();
 }
 
