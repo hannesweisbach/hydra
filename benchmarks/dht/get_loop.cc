0741001e72e08515155b96e782d9d75ab0e22ed5
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-04 17:35:58 +0200

    Update get benchmark

--- benchmarks/dht/get_loop.cc
+++ benchmarks/dht/get_loop.cc
@@ -3,24 +3,32 @@
 #include <atomic>
 #include <thread>
 
-#include "hydra/client.h"
-#include "hydra/hash.h"
-
-#include "util/Logger.h"
-
-#include "google/profiler.h"
+#include "protocol/message.h"
+#include "rdma/RDMAClientSocket.h"
 
 typedef std::unique_ptr<unsigned char, std::function<void(unsigned char *)> >
 value_ptr;
 
 hydra::node_info get_info(const RDMAClientSocket &socket) {
-  auto init = socket.recv_async<mr_response>();
-  socket.sendImmediate(init_request());
+  auto init = socket.recv_async<kj::FixedArray<capnp::word, 9> >();
+
+  ::capnp::MallocMessageBuilder request;
+  request.initRoot<hydra::protocol::DHTRequest>().setInit();
+  kj::Array<capnp::word> serialized = messageToFlatArray(request);
+
+  socket.sendImmediate(serialized.begin(),
+                       serialized.size() * sizeof(capnp::word));
 
   init.first.get(); // block.
-  auto mr = init.second.first->value();
-  assert(mr.size >= sizeof(hydra::node_info));
-  auto info = socket.read<hydra::node_info>(mr.addr, mr.rkey);
+
+  auto reply = capnp::FlatArrayMessageReader(*init.second.first);
+  auto reader = reply.getRoot<hydra::protocol::DHTResponse>();
+
+  assert(reader.which() == hydra::protocol::DHTResponse::INIT);
+
+  auto mr = reader.getInit().getInfo();
+  assert(mr.getSize() >= sizeof(hydra::node_info));
+  auto info = socket.read<hydra::node_info>(mr.getAddr(), mr.getRkey());
   info.first.get(); // block
 
   return *info.second.first;
@@ -32,15 +40,16 @@ std::pair<value_ptr, const size_t> find_entry(const RDMAClientSocket &socket,
                                               const hydra::node_info &info) {
   const size_t table_size = info.table_size;
   const RDMAObj<hydra::hash_table_entry> *remote_table =
-      static_cast<const RDMAObj<hydra::hash_table_entry> *>(info.key_extents.addr);
+      static_cast<const RDMAObj<hydra::hash_table_entry> *>(
+          info.key_extents.addr);
   const uint32_t rkey = info.key_extents.rkey;
 
-  const size_t index = hydra::hash(key, key_length) % table_size;
+  size_t index = hydra::hash(key, key_length) % table_size;
 
   auto mem = socket.from(&remote_table[index], info.key_extents.rkey);
   auto &entry = mem.first->get();
 
-  for (size_t hop = entry.hop, d = 1; hop; hop >>= 1, d++) {
+  for (size_t hop = entry.hop; hop; hop >>= 1) {
     if ((hop & 1) && !entry.is_empty() && (key_length == entry.key_length())) {
       auto data = socket.malloc<unsigned char>(entry.ptr.size);
       uint64_t crc = 0;
@@ -53,42 +62,17 @@ std::pair<value_ptr, const size_t> find_entry(const RDMAClientSocket &socket,
         return { std::move(data.first), entry.value_length() };
       }
     }
-    const size_t next_index = (index + d) % table_size;
-    socket.reload(mem, &remote_table[next_index], rkey);
+    socket.reload(mem, &remote_table[++index % table_size], rkey);
   }
 
   return { value_ptr(nullptr, [](void *) {}), 0 };
 }
 
-struct data {
-  rdma_ptr<unsigned char> key;
-  rdma_ptr<unsigned char> value;
-  size_t key_length;
-  size_t value_length;
-  std::pair<std::future<qp_t>, rdma_ptr<put_response> > result;
-  data(rdma_ptr<unsigned char> key, rdma_ptr<unsigned char> value,
-       const size_t key_length, const size_t value_length)
-      : key(std::move(key)), value(std::move(value)), key_length(key_length),
-        value_length(value_length) {}
-};
-
-static void send(RDMAClientSocket &socket, data &request) {
-  put_request r = { { request.key.first.get(), request.key_length,
-                      request.key.second->rkey },
-                    { request.value.first.get(), request.value_length,
-                      request.value.second->rkey } };
-
-  request.result = socket.recv_async<put_response>();
-  socket.sendImmediate(r);
-}
-
 static void get_keys(RDMAClientSocket &socket, const size_t max_keys,
-                     std::atomic_bool &run, std::atomic<uint64_t> &cnt) {
+                     std::atomic_bool &run, std::atomic<uint64_t> &found,
+                     std::atomic<uint64_t> &notfound) {
   const hydra::node_info info = get_info(socket);
-
   std::vector<std::string> keys;
-  std::vector<data> requests;
-  requests.reserve(max_keys);
 
   for (size_t i = 0; i < max_keys; i++) {
     std::ostringstream ss;
@@ -101,32 +85,55 @@ static void get_keys(RDMAClientSocket &socket, const size_t max_keys,
     if (i >= keys.size())
       i = 0;
     const auto &key = keys[i];
-    find_entry(socket, reinterpret_cast<const unsigned char *>(key.c_str()),
-               key.size(), info);
-    cnt++;
+    auto result =
+        find_entry(socket, reinterpret_cast<const unsigned char *>(key.c_str()),
+                   key.size(), info);
+    if (result.first) {
+      found++;
+    } else {
+      notfound++;
+    }
   }
 }
 
 int main() {
-  std::atomic<uint64_t> cnt(0);
   const size_t max_keys = 512;
+  const size_t from_threads = 1;
+  const size_t to_threads = 20;
+  std::atomic<uint64_t> found(0);
+  std::atomic<uint64_t> notfound(0);
   const auto measurement_time = std::chrono::seconds(20);
 
-  RDMAClientSocket socket("10.1", "8042");
-  socket.connect();
+  for (size_t current_threads = from_threads; current_threads < to_threads;
+       current_threads++) {
+    RDMAClientSocket socket("10.1", "8042");
+    socket.connect();
 
-  //  ProfilerStart("./load.prof");
-  // load_keys(client, max_keys, 64);
-  std::atomic_bool run(true);
-  hydra::async([&]() {
-    std::this_thread::sleep_for(measurement_time);
-    run = false;
-  });
-  get_keys(socket, max_keys, run, cnt);
-  const uint64_t seconds = std::chrono::duration_cast<std::chrono::seconds>(
-      measurement_time).count();
-  log_info() << "kOps/s: " << cnt.fetch_and(0) / seconds / 1000;
+    std::cout << "Running with " << current_threads << " thread(s) ... ";
+    std::cout.flush();
 
-  ProfilerStop();
+    std::atomic_bool run(true);
+    hydra::async([&]() {
+      std::this_thread::sleep_for(measurement_time);
+      run = false;
+    });
+
+    std::vector<std::thread> threads;
+    for (size_t i = 0; i < current_threads; i++) {
+      threads.emplace_back(get_keys, std::ref(socket), max_keys, std::ref(run),
+                           std::ref(found), std::ref(notfound));
+    }
+
+    for (auto &&thread : threads)
+      thread.join();
+
+    const uint64_t seconds = std::chrono::duration_cast<std::chrono::seconds>(
+        measurement_time).count();
+    const uint64_t keys = found.load() + notfound.load();
+    std::cout << keys / seconds / 1000 << " kOps/s" << std::endl;
+    log_info() << "Found " << found.load() << " keys of " << keys;
+    found = 0;
+    notfound = 0;
+  }
 }
 
