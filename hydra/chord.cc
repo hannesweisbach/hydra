3081b29787e46720242cfa00babb3f3ee7555899
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-22 17:47:57 +0200

    Add hydra::chord::node class

--- hydra/chord.cc
+++ hydra/chord.cc
@@ -1,6 +1,7 @@
 #include "hydra/chord.h"
 #include "hydra/types.h"
 #include "hydra/client.h"
+#include "hydra/protocol/message.h"
 
 namespace hydra {
 namespace chord {
@@ -30,5 +31,52 @@ node_id predecessor(const routing_table &start, const keyspace_t &id) {
 node_id successor(const routing_table &start, const keyspace_t &id) {
   return find_table(start, id).successor().node;
 }
+
+node::node(const std::string &host, const std::string &port)
+    : RDMAClientSocket(host, port) {
+  connect();
+
+  auto response = recv_async<kj::FixedArray<capnp::word, 9> >();
+
+  send(chord_request());
+  response.first.get();
+
+  auto message = capnp::FlatArrayMessageReader(*response.second.first);
+  auto reader = message.getRoot<protocol::DHTResponse>();
+  assert(reader.which() == hydra::protocol::DHTResponse::CHORD);
+  auto t = reader.getChord().getTable();
+  table_mr.addr = t.getAddr();
+  table_mr.size = t.getSize();
+  table_mr.rkey = t.getRkey();
+}
+
+hydra::routing_table node::load_table() const {
+  auto table =
+      read<RDMAObj<hydra::routing_table> >(table_mr.addr, table_mr.rkey);
+  table.first.get();
+  return table.second.first->get();
+}
+
+hydra::routing_table node::find_table(const keyspace_t &id) const {
+  using namespace hydra::literals;
+
+  auto table = load_table();
+  while (!id.in(table.self().node.id + 1_ID, table.successor().node.id)) {
+    auto re = table.preceding_node(id).node;
+    node node(re.ip, re.port);
+
+    table = node.load_table();
+  }
+  return table;
+}
+
+node_id node::predecessor(const keyspace_t &id) const {
+  return find_table(id).self().node;
+}
+
+node_id node::successor(const keyspace_t &id) const {
+  return find_table(id).successor().node;
 }
 }
+}
+
--- hydra/chord.h
+++ hydra/chord.h
@@ -1,11 +1,25 @@
 #pragma once
 
+#include <string>
+
+#include "rdma/RDMAClientSocket.h"
 #include "hydra/types.h"
 
 namespace hydra {
 namespace chord {
 node_id predecessor(const routing_table &start, const keyspace_t &id);
 node_id successor(const routing_table &start, const keyspace_t &id);
+
+class node : public virtual RDMAClientSocket {
+public:
+  node(const std::string &host, const std::string &port);
+  node_id predecessor(const keyspace_t &id) const;
+  node_id successor(const keyspace_t &id) const;
+private:
+  hydra::routing_table load_table() const;
+  hydra::routing_table find_table(const keyspace_t &) const;
+  mr table_mr;
+};
 }
 }
 
