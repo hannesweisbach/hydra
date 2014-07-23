9a071863f09460e87201b398aba791e468ba920b
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-23 18:46:19 +0200

    Use load for RDMAObj in passive

--- hydra/passive.cpp
+++ hydra/passive.cpp
@@ -110,9 +110,7 @@ hydra::passive::find_entry(const std::vector<unsigned char> &key) {
   uintptr_t remote_index = table_base + index * entry_size;
 
   auto mem = heap.malloc<RDMAObj<hash_table_entry> >();
-  do {
-    read(*mem.first, mem.second, remote_index, info->key_extents.rkey);
-  } while (!mem.first->valid());
+  hydra::rdma::load(*this, *mem.first, mem.second, remote_index, rkey);
   auto &entry = mem.first->get();
 
   for (size_t hop = entry.hop, d = 1; hop; hop >>= 1, d++) {
@@ -133,9 +131,7 @@ hydra::passive::find_entry(const std::vector<unsigned char> &key) {
     }
     const size_t next_index = (index + d) % table_size;
     remote_index = table_base + next_index * entry_size;
-    do {
-      read(*mem.first, mem.second, remote_index, info->key_extents.rkey);
-    } while (!mem.first->valid());
+    hydra::rdma::load(*this, *mem.first, mem.second, remote_index, rkey);
   }
 
   return value;
