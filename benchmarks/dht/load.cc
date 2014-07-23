5fe788ec32891b3194cf9b11028e7f9f65fa484c
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-23 14:53:47 +0200

    Cleanup

--- benchmarks/dht/load.cc
+++ benchmarks/dht/load.cc
@@ -2,59 +2,6 @@
 #include <sstream>
 #include <vector>
 
-#if 0
-#include "hydra/client.h"
-#include "hydra/hash.h"
-#include "util/Logger.h"
-
-void load_keys(hydra::client &client, const size_t max_keys,
-               size_t value_length) {
-  std::mt19937_64 generator;
-  std::uniform_int_distribution<unsigned char> distribution(' ', '~');
-  
-  struct data {
-    std::string key;
-    std::unique_ptr<unsigned char[]> value;
-    std::future<bool> result;
-    data(const std::string &key, std::unique_ptr<unsigned char[]> value)
-        : key(key), value(std::move(value)) {}
-  };
-
-  std::vector<data> requests;
-  requests.reserve(max_keys);
-
-  for (size_t i = 0; i < max_keys; i++) {
-    std::ostringstream ss;
-    ss << hydra::hash(&i, sizeof(i));
-    std::unique_ptr<unsigned char[]> value =
-#if 1
-        std::unique_ptr<unsigned char[]>(new unsigned char[value_length]);
-#else
-        std::make_unique<unsigned char[]>(value_length);
-#endif
-    for (size_t byte = 0; byte < value_length; byte++) {
-      value[byte] = distribution(generator);
-    }
-
-    requests.emplace_back(ss.str(), std::move(value));
-  }
-
-  auto start = std::chrono::high_resolution_clock::now();
-
-  for (auto &&request : requests) {
-    request.result =
-        client.add(reinterpret_cast<const unsigned char *>(request.key.c_str()),
-                   request.key.size(), request.value.get(), value_length);
-    //request.result.wait();
-  }
-
-  size_t i = 0;
-  for (auto &&request : requests) {
-    request.result.get();
-    //log_info() << "Finished " << i++;
-}
-#else
-
 #include "rdma/RDMAClientSocket.h"
 #include "protocol/message.h"
 
@@ -80,7 +27,7 @@ static void load_keys(RDMAClientSocket &socket, const size_t max_keys,
   requests.reserve(max_keys);
 
   std::vector<buffer_t> buffers(max_keys);
-  mr_t buffer_mr(socket.register_memory(ibv_access::MSG, buffers));
+  auto buffer_mr = socket.register_memory(ibv_access::MSG, buffers);
 
   for (size_t i = 0; i < max_keys; i++) {
     std::ostringstream ss;
@@ -109,12 +56,9 @@ static void load_keys(RDMAClientSocket &socket, const size_t max_keys,
     socket.send(serialized);
   }
 
-#if 1
-  size_t i = 0;
   for (auto &&request : requests) {
     request.result.get();
   }
-#endif
 
   auto end = std::chrono::high_resolution_clock::now();
   auto duration = end - start;
@@ -137,15 +81,12 @@ static void load_keys(RDMAClientSocket &socket, const size_t max_keys,
   }
 
 }
-#endif
 
 int main() {
-  const size_t max_keys = 1024;
+  const size_t max_keys = 128;
 
-  //hydra::client client("10.1", "8042");
   RDMAClientSocket socket("10.1", "8042");
   socket.connect();
 
-  //load_keys(client, max_keys, 64);
   load_keys(socket, max_keys, 64);
 }
