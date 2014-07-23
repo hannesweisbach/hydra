d64297973cdd828d93142b0ae4e92ace5020e429
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2014-07-23 10:47:22 +0200

    Migrate get benchmark to hydra::passive

--- benchmarks/dht/get_loop.cc
+++ benchmarks/dht/get_loop.cc
@@ -2,69 +2,34 @@
 #include <sstream>
 #include <atomic>
 #include <thread>
+#include <string>
+#include <vector>
 
-#include "hydra/client.h"
-#include "protocol/message.h"
-#include "rdma/RDMAClientSocket.h"
+#include "hydra/passive.h"
 
-typedef std::unique_ptr<unsigned char, std::function<void(unsigned char *)> >
-value_ptr;
-
-std::pair<value_ptr, const size_t> find_entry(const RDMAClientSocket &socket,
-                                              const unsigned char *key,
-                                              const size_t key_length,
-                                              const hydra::node_info &info) {
-  const size_t table_size = info.table_size;
-  const RDMAObj<hydra::hash_table_entry> *remote_table =
-      static_cast<const RDMAObj<hydra::hash_table_entry> *>(
-          info.key_extents.addr);
-  const uint32_t rkey = info.key_extents.rkey;
-
-  size_t index = hydra::hash(key, key_length) % table_size;
-
-  auto mem = socket.from(&remote_table[index], info.key_extents.rkey);
-  auto &entry = mem.first->get();
-
-  for (size_t hop = entry.hop; hop; hop >>= 1) {
-    if ((hop & 1) && !entry.is_empty() && (key_length == entry.key_length())) {
-      auto data = socket.malloc<unsigned char>(entry.ptr.size);
-      uint64_t crc = 0;
-      do {
-        socket.read(data.first.get(), data.second, entry.key(), entry.rkey,
-                    entry.ptr.size).get();
-        crc = hydra::hash64(data.first.get(), entry.ptr.size);
-      } while (entry.ptr.crc != crc);
-      if (memcmp(data.first.get(), key, key_length) == 0) {
-        return { std::move(data.first), entry.value_length() };
-      }
-    }
-    socket.reload(mem, &remote_table[++index % table_size], rkey);
-  }
-
-  return { value_ptr(nullptr, [](void *) {}), 0 };
-}
-
-static void get_keys(RDMAClientSocket &socket, const size_t max_keys,
-                     std::atomic_bool &run, std::atomic<uint64_t> &found,
+static void get_keys(const std::string &host, const std::string &port,
+                     const size_t max_keys, std::atomic_bool &run,
+                     std::atomic<uint64_t> &found,
                      std::atomic<uint64_t> &notfound) {
-  const hydra::node_info info = hydra::get_info(socket);
-  std::vector<std::string> keys;
+  using key_t = std::vector<unsigned char>;
+  hydra::passive socket(host, port);
+  std::vector<key_t> keys;
 
   for (size_t i = 0; i < max_keys; i++) {
     std::ostringstream ss;
     ss << std::setw(4) << i;
 
-    keys.push_back(ss.str());
+    auto str = ss.str();
+    keys.emplace_back(
+        reinterpret_cast<const unsigned char *>(str.data()),
+        reinterpret_cast<const unsigned char *>(str.data() + str.size()));
   }
 
   for (size_t i = 0; run.load(); i++) {
     if (i >= keys.size())
       i = 0;
     const auto &key = keys[i];
-    auto result =
-        find_entry(socket, reinterpret_cast<const unsigned char *>(key.c_str()),
-                   key.size(), info);
-    if (result.first) {
+    if (socket.contains(key)) {
       found++;
     } else {
       notfound++;
@@ -82,9 +47,6 @@ int main() {
 
   for (size_t current_threads = from_threads; current_threads < to_threads;
        current_threads++) {
-    RDMAClientSocket socket("10.1", "8042");
-    socket.connect();
-
     std::cout << "Running with " << current_threads << " thread(s) ... ";
     std::cout.flush();
 
@@ -96,7 +58,7 @@ int main() {
 
     std::vector<std::thread> threads;
     for (size_t i = 0; i < current_threads; i++) {
-      threads.emplace_back(get_keys, std::ref(socket), max_keys, std::ref(run),
+      threads.emplace_back(get_keys, "10.1", "8042", max_keys, std::ref(run),
                            std::ref(found), std::ref(notfound));
     }
 
