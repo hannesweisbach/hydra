f9f511ddd3783ee245ad81e1bf4149d8a5bf5bb1
Hannes Weisbach <hannes.weisbach@mailbox.tu-dresden.de>
2015-03-02 18:09:30 +0100

    Improved benchmark of small puts

--- benchmarks/dht/put_loop_inline.cc
+++ benchmarks/dht/put_loop_inline.cc
@@ -4,38 +4,56 @@
 #include <thread>
 #include <algorithm>
 #include <memory>
+#include <chrono>
+#include <algorithm>
+#include <vector>
 
 #include "protocol/message.h"
 #include "rdma/RDMAClientSocket.h"
 
+#ifdef PROFILER
+#include <gperftools/profiler.h>
+#endif
+
+#include "util/stat.h"
+
+using response = kj::FixedArray<capnp::word, 9>;
+
+static thread_local mr_t response_mr;
+
 struct data {
-  std::vector<unsigned char> kv;
-  uint8_t key_size;
-  std::unique_ptr<kj::FixedArray<capnp::word, 9> > response;
-  mr_t response_mr;
+  kj::Array<capnp::word> request;
+  response *response;
 };
 
-static void send(RDMAClientSocket &socket, data &request) {
-  auto result = socket.recv_async(*request.response, request.response_mr.get());
+static ecdf measurement;
 
-  socket.send(put_message_inline(request.kv, request.key_size));
+static void send(RDMAClientSocket &socket, data &request) {
+  auto result = socket.recv_async(*request.response, response_mr.get());
+  socket.send(request.request);
+
+  // measurement.start();
   result.get();
+  // measurement.end();
 
   auto reply = capnp::FlatArrayMessageReader(*request.response);
-  auto reader = reply.getRoot<hydra::protocol::DHTResponse>();
 
-  assert(reader.getAck().getSuccess());
+  assert(reply.getRoot<hydra::protocol::DHTResponse>().getAck().getSuccess());
 }
 
 static void load_keys(const std::string &host, const std::string &port,
-                      const size_t max_keys, const size_t value_length,
-                      std::atomic_bool &run, std::atomic<uint64_t> &cnt) {
+                      const size_t max_keys, const size_t value_length) {
   RDMAClientSocket socket(host, port);
   socket.connect();
   std::mt19937_64 generator;
   std::uniform_int_distribution<unsigned char> distribution(' ', '~');
 
+  std::vector<response> responses(max_keys);
+  response_mr = socket.register_memory(ibv_access::READ, responses.data(),
+                                       responses.size() * sizeof(response));
+
   std::vector<data> requests;
+  requests.reserve(max_keys);
 
   for (size_t i = 0; i < max_keys; i++) {
     std::ostringstream ss;
@@ -48,49 +66,71 @@ static void load_keys(const std::string &host, const std::string &port,
     assert(key_length + value_length < 256);
 
     std::generate_n(std::back_inserter(kv), value_length,
-                    std::bind(distribution, generator));
+                    [&]() { return distribution(generator); });
 
-    requests.push_back({ kv,
-                         static_cast<uint8_t>(key_length),
-                         std::make_unique<kj::FixedArray<capnp::word, 9> >(),
-                         {} });
-    requests.back().response_mr =
-        socket.register_memory(ibv_access::READ, *requests.back().response);
+    requests.emplace_back(
+        data{ put_message_inline(kv, static_cast<uint8_t>(key_length)),
+              &responses.at(i) });
   }
 
-  for (;;) {
-    for (auto &&request : requests) {
-      send(socket, request);
-      cnt++;
-      if (!run)
-        return;
-    }
+  auto start = std::chrono::high_resolution_clock::now();
+
+#ifdef PROFILER
+  ProfilerStart("./put_inline.profile");
+#endif
+
+  for (auto &&request : requests) {
+    send(socket, request);
   }
+
+#ifdef PROFILER
+  ProfilerStop();
+#endif
+
+  auto end = std::chrono::high_resolution_clock::now();
+
+  auto duration = end - start;
+
+  size_t ops = 0;
+  auto time =
+      std::chrono::duration_cast<std::chrono::nanoseconds>(duration).count();
+
+  std::cout << time << "ns" << std::endl;
+
+  auto seconds =
+      std::chrono::duration_cast<std::chrono::seconds>(duration).count();
+  auto ms =
+      std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+  auto us =
+      std::chrono::duration_cast<std::chrono::microseconds>(duration).count();
+
+  if (seconds > 100) {
+    ops = max_keys / seconds;
+  } else if (ms > 100) {
+    ops = max_keys * 1000 / ms;
+    std::cout << "ms " << ms << std::endl;
+  } else if (us > 100) {
+    ops = max_keys * 1000 * 1000 / us;
+  }
+
+  std::cout << ops << " Ops/s" << std::endl;
 }
 
 int main(int argc, char const *const argv[]) {
-  const size_t value_length = (argc < 2) ? 64 : atoi(argv[1]);
-  const size_t max_keys = 255;
+  const size_t max_keys = (argc < 2) ? 1000 * 300 * 1 : atoi(argv[1]);
+  const size_t value_length = (argc < 3) ? 64 : atoi(argv[2]);
   const size_t min_threads = 1;
-  const size_t max_threads = 20;
-  const auto measurement_time = std::chrono::seconds(2);
+  const size_t max_threads = 1;
 
+  std::cout << max_keys << " keys with " << value_length << " bytes."
+            << std::endl;
   static_assert(min_threads <= max_threads,
                 "The minimum number of threads must "
                 "be smaller than the maximum number");
 
-  std::atomic<uint64_t> cnt(0);
-
   for (const auto &value_size : { value_length /*64, 128, 256, 1024, 4096*/ }) {
     for (size_t cur_threads = min_threads; cur_threads <= max_threads;
          cur_threads++) {
-      std::atomic_bool run(true);
-
-      hydra::async([&]() {
-        std::this_thread::sleep_for(measurement_time);
-        run = false;
-      });
-
       std::cout << "Running with a value_size of " << value_size << " and "
                 << cur_threads << " threads ... ";
       std::cout.flush();
@@ -99,18 +139,15 @@ int main(int argc, char const *const argv[]) {
       threads.reserve(cur_threads);
 
       std::generate_n(std::back_inserter(threads), cur_threads, [&]() {
-        return std::thread(load_keys, "10.1", "8042", max_keys, value_length,
-                           std::ref(run), std::ref(cnt));
+        return std::thread(load_keys, "10.10", "8042", max_keys, value_length);
       });
 
       for (auto &&thread : threads) {
         thread.join();
       }
-
-      const uint64_t seconds = std::chrono::duration_cast<std::chrono::seconds>(
-          measurement_time).count();
-      std::cout << "kOps/s: " << cnt.fetch_and(0) / seconds / 1000 << std::endl;
     }
   }
+
+  std::cout << measurement << std::endl;
 }
 
